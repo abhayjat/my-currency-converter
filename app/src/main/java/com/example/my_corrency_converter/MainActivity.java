ed6b6888ab1b9c413116fa26ab2e4edb2f0bb86a
abhayjat <abhay.jat@outlook.com>
2020-08-16 14:19:05 +0530

    Update Input Output

--- app/src/main/java/com/example/my_corrency_converter/MainActivity.java
+++ app/src/main/java/com/example/my_corrency_converter/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,11 +29,11 @@ public class MainActivity extends AppCompatActivity {
         sp2=(Spinner) findViewById(R.id.tospinner);
         btn=(Button) findViewById(R.id.convert);
 
-        String[] from={"USD"};
+        String[] from={"Indian Rupee","USD"};
         ArrayAdapter adfrom=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,from);
         sp1.setAdapter(adfrom);
 
-        String[] to={"Indian Rupee","Bitcoin","Chinese Yuan","Pakistani Rupee","USD"};
+        String[] to={"Bitcoin","Chinese Yuan","Indian Rupee","Pakistani Rupee","USD"};
         ArrayAdapter adto=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,to);
         sp2.setAdapter(adto);
 
@@ -48,27 +50,81 @@ public class MainActivity extends AppCompatActivity {
                         Toast toast=Toast.makeText(getApplicationContext(),"Enter Amount",Toast.LENGTH_SHORT);
                         toast.show();
                 }
+                if(sp1.getSelectedItem().toString() == "USD"){
 
-                if(sp1.getSelectedItem().toString() == "USD" && sp2.getSelectedItem().toString()=="Indian Rupee"){
-                    tot = amount * 74.89;
-                    Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
-                    toast.show();
-                }else if(sp1.getSelectedItem().toString() == "USD" && sp2.getSelectedItem().toString()=="Bitcoin"){
-                    tot = amount * 0.000085;
-                    Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
-                    toast.show();
-                }else if(sp1.getSelectedItem().toString() == "USD" && sp2.getSelectedItem().toString()=="Pakistani Rupee"){
-                    tot = amount * 168.08;
-                    Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
-                    toast.show();
-                }else if(sp1.getSelectedItem().toString() == "USD" && sp2.getSelectedItem().toString()=="Chinese Yuan"){
-                    tot = amount * 6.95;
-                    Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
-                    toast.show();
+                    if(sp2.getSelectedItem().toString()=="Indian Rupee"){
+                        tot = amount * 74.89;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else if(sp2.getSelectedItem().toString()=="Bitcoin"){
+                        tot = amount * 0.000085;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else if(sp2.getSelectedItem().toString()=="Pakistani Rupee"){
+                        tot = amount * 168.08;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else if(sp2.getSelectedItem().toString()=="Chinese Yuan"){
+                        tot = amount * 6.95;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else if(sp2.getSelectedItem().toString()=="USD"){
+                        tot = amount * 1;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else{
+                        Toast toast=Toast.makeText(getApplicationContext(),"Not Convert",Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }
+
+
+
+
+                }else if(sp1.getSelectedItem().toString() == "Indian Rupee"){
+                    if(sp2.getSelectedItem().toString()=="Indian Rupee"){
+                        tot = amount * 1;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else if(sp2.getSelectedItem().toString()=="Bitcoin"){
+                        tot = amount * 0.0000011;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else if(sp2.getSelectedItem().toString()=="Pakistani Rupee"){
+                        tot = amount * 2.25;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else if(sp2.getSelectedItem().toString()=="Chinese Yuan"){
+                        tot = amount * 0.093;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }else if(sp2.getSelectedItem().toString()=="USD"){
+                        tot = amount * 0.013;
+                        Toast toast=Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }
+                    else{
+                        Toast toast=Toast.makeText(getApplicationContext(),"Not Convert",Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
+                        toast.show();
+                    }
+
                 }else{
                     Toast toast=Toast.makeText(getApplicationContext(),"Not Convert",Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL,0,80);
                     toast.show();
                 }
+
             }
         });
     }
